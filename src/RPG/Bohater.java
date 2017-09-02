f319af0b5188677bcc5cd5d397cb2ef72e076a61
MariAntonina <maryhuzarek@gmail.com>
2017-09-02 18:24:48 +0200

    zostane kiedys bohaterem

--- src/RPG/Bohater.java
+++ src/RPG/Bohater.java
@@ -10,13 +10,17 @@ public class Bohater {
     private Miecz _miecz;
     private Plecak _plecak;
 
-    public Bohater(String imie, Tarcza tarcza, Miecz miecz, Plecak plecak ){
-         _imie = imie;
-         _tarcza = tarcza;
-         _miecz = miecz;
-         _plecak = plecak;
+    public Bohater(String imie, Tarcza tarcza, Miecz miecz, Plecak plecak ) {
+        _imie = imie;
+        _tarcza = tarcza;
+        _miecz = miecz;
+        _plecak = plecak;
     }
 
-
+    public String toString() {
+     return  String.format("Bohater [ %s, Ubrany jest w %s, %.0f,\n%s, %.0f, \n%s, %.0f]",
+                _imie, _tarcza.pobierzOpis(), _tarcza.obrona(),
+                _miecz.pobierzOpis(), _miecz.atak(),
+                _plecak.pobierzOpis(), _plecak.pojemnosc());}
 }
 
--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -6,15 +6,25 @@ package RPG;
 public class Main {
     public static void main(String[] args) {
 
-        Tarcza tarcza = new TarczaZZolwiaTeodora();
+        TarczaZZolwiaTeodora tarcza = new TarczaZZolwiaTeodora();
+        StalowaKlataDavidaHasselhoffa skdh = new StalowaKlataDavidaHasselhoffa(tarcza);
+        BuforyPameliA bpa = new BuforyPameliA(skdh);
+
         Miecz miecz = new DzikaTrabaMamuta();
-        Plecak plecak = new ZielarskiPlecakWloczykija();
-        Bohater Czeslaw = new Bohater("Czesław", tarcza, miecz, plecak);
+        KielProsiaczka kp = new KielProsiaczka(miecz);
+        BrzytwaHerkulesa bh = new BrzytwaHerkulesa(kp);
 
-        System.out.printf("Nazwa: %s :: Zawartosc alkoholu %s, %s , %s",
-                Czeslaw);
+        ZielarskiPlecakWloczykija plecak = new ZielarskiPlecakWloczykija();
+        KociolGargamela kg = new KociolGargamela(plecak);
+        DmuchanyBicepsHardkorowegoKoksa dbhk = new DmuchanyBicepsHardkorowegoKoksa(kg);
 
+        Bohater Czeslaw = new Bohater("Czesław", skdh, kp, kg);
 
-
+//        System.out.println(Czeslaw.toString() + " \n"+
+//                bpa.pobierzOpis() + " " + bpa.obrona() + " \n"
+//                + bh.pobierzOpis() + " " + bh.atak() + " \n"
+//                + dbhk.pobierzOpis() + " " + dbhk.pojemnosc());
+//    }
+         System.out.println( Czeslaw.toString());
     }
 }
